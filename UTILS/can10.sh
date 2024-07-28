aea6b2b0cb8aea310562b54d5653d224c2375cfa
Ozuba <miguelozubas@gmail.com>
2024-07-28 14:12:05 +0200

    Removed Utils folder and added comments to nodes and re formated LVBMS

--- UTILS/can10.sh
+++
@@ -1,3 +0,0 @@
-sudo ip link set can0 down
-sudo ip link set can0 up type can bitrate 1000000
-cantools monitor TER.dbc -c can0
--- UTILS/can5.sh
+++
@@ -1,3 +0,0 @@
-sudo ip link set can0 down
-sudo ip link set can0 up type can bitrate 500000
-cantools monitor INVERTER.dbc -c can0
